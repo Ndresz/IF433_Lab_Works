42224099298340a881731d430a2ccd73711304d0
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-02-23 21:18:27 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000111047_AndreasRahardian/week03/Employee.kt
+++ src/oop_00000111047_AndreasRahardian/week03/Employee.kt
@@ -2,4 +2,8 @@ package oop_00000111047_AndreasRahardian.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba gaji ke: $value")
+            this.salary = value
+        }
 }
---
+++ src/oop_00000111047_AndreasRahardian/week03/main.kt
@@ -0,0 +1,7 @@
+package oop_00000111047_AndreasRahardian.week03
+
+fun main(args: Array<String>) {
+    val e =     Employee ("budi")
+    println("---Test Error---")
+    e.salary = 5000
+}
