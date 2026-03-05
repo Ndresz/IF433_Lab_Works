f315d23d23dc34243dfd350c33fafce2c4c37d62
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-05 23:48:03 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000111047_AndreasRahardian/week04/Main.kt
@@ -0,0 +1,15 @@
+package oop_00000111047_AndreasRahardian.week04
+
+fun main() {
+
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle(brand = "Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car(brand = "Toyota", numberOfDoors = 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
