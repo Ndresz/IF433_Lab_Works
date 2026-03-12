6c2ddaa0cea9e61368ae62c852f64a0a1021217e
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-12 23:19:44 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_00000111047_AndreasRahardian/week05/EWallet.kt
@@ -0,0 +1,17 @@
+package oop_00000111047_AndreasRahardian.week05
+
+class EWallet(accountName: String, var balance: Double) : PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (balance >= amount) {
+            balance -= amount
+            println("Pembayaran berhasil. Saldo tersisa: $balance")
+        } else {
+            println("Saldo tidak cukup")
+        }
+    }
+
+    fun topUp(amount: Double) {
+        balance += amount
+        println("Top up berhasil. Saldo saat ini: $balance")
+    }
+}
