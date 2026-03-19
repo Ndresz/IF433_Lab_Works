7ac55fc940fb4b6a382714878ec8dc1bd054c8da
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-19 22:40:10 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000111047_AndreasRahardian/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000111047_AndreasRahardian.week06
+
+interface Camera {
+    fun turnon() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnon() { println("Sinyal seluler mencari jaringan.") }
+}
