a68521d64aa8ef5f070e89eec217be19a6c68603
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-27 19:47:57 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_00000111047_AndreasRahardian/week07/Main.kt
+++ src/oop_00000111047_AndreasRahardian/week07/Main.kt
@@ -35,4 +35,8 @@ fun main() {
         Loading -> "Tampilkan Spinner"
     }
     println(uiMessage)
+
+    println("\n=== SIMULASI GAME RPG ===")
+    GameManager.startGame()
+    GameManager.startGame()
 }
