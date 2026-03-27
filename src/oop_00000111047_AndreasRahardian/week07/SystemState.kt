4989db3f47309a5bcf3963b2675e431e23870b98
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-27 19:37:09 +0700

    week07: create AppState enum

---
+++ src/oop_00000111047_AndreasRahardian/week07/SystemState.kt
@@ -0,0 +1,10 @@
+package oop_00000111047_AndreasRahardian.week07
+
+enum class AppState {
+    STARTING, RUNNING, STOPPED
+}
+
+sealed class ApiResponse
+data class Success(val data: String) : ApiResponse()
+data class Error(val message: String) : ApiResponse()
+object Loading : ApiResponse()
