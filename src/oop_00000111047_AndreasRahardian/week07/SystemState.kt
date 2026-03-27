f01b703755bd2e6357ec119584755f741492cb83
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-03-27 19:41:59 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000111047_AndreasRahardian/week07/SystemState.kt
+++ src/oop_00000111047_AndreasRahardian/week07/SystemState.kt
@@ -5,6 +5,6 @@ enum class AppState {
 }
 
 sealed class ApiResponse
-data class Success(val data: String) : ApiResponse()
-data class Error(val message: String) : ApiResponse()
-object Loading : ApiResponse()
+data class Success (val data: String): ApiResponse()
+data class Error(val message: String): ApiResponse()
+object Loading: ApiResponse()
