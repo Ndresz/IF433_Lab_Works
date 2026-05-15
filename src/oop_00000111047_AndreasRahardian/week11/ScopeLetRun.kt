9514cedc39c75f81dc417be2476d7e8d92687d45
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-05-15 23:00:31 +0700

    week11: implement run function for object computation

--- src/oop_00000111047_AndreasRahardian/week11/ScopeLetRun.kt
+++ src/oop_00000111047_AndreasRahardian/week11/ScopeLetRun.kt
@@ -1,4 +1,19 @@
 package oop_00000111047_AndreasRahardian.week11
 
-class ScopeLetRun {
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    val length = name?.let {
+        println("Nama terdeteksi: $it")
+        it.length
+    }
+    println("Panjang nama: $length")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}") // 'this' bisa dihilangkan (implicit)
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
 }
