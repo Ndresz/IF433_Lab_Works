9cd9ac7a13e06ec6045ed0d53ae62fd4be584738
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-05-15 22:38:34 +0700

    week10: implement generic function with return type

--- src/oop_00000111047_AndreasRahardian/week10/GenericMethods.kt
+++ src/oop_00000111047_AndreasRahardian/week10/GenericMethods.kt
@@ -1,4 +1,9 @@
 package oop_00000111047_AndreasRahardian.week10
 
-class GenericMethods {
+fun <T> printData(data: T) {
+    println("Data yang diterima: $data")
+}
+
+fun <T> processData(input: T): T {
+    return input
 }
