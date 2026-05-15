c05a821d973dabf2aac5e07d3acb9493c52863d2
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-05-15 22:42:34 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000111047_AndreasRahardian/week10/WalletRepository.kt
+++ src/oop_00000111047_AndreasRahardian/week10/WalletRepository.kt
@@ -1,4 +1,17 @@
 package oop_00000111047_AndreasRahardian.week10
 
-class WalletRepository {
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+
+    fun findByName(nameQuery: String): T? {
+        return items.find { (it as? Coin)?.name == nameQuery }
+    }
 }
