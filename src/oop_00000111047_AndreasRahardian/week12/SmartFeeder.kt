5b8b1a3e9e1fc830ac64e5509f9f05259043f4ee
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-05-16 12:35:23 +0700

    Week 12: (task) implement hardware validatoin with throw

--- src/oop_00000111047_AndreasRahardian/week12/SmartFeeder.kt
+++ src/oop_00000111047_AndreasRahardian/week12/SmartFeeder.kt
@@ -5,3 +5,6 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (isJammed) {
         throw DispenserJamException()
     }
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
