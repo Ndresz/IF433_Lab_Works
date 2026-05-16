e7ce0f41bb1c0167b5eeb56db2297317dfda6e19
KazukoSyame <andreas.r.silitonga@gmail.com>
2026-05-16 13:06:51 +0700

    Week 12: (task) test full robust smart feeder pipeline

--- src/oop_00000111047_AndreasRahardian/week12/SmartFeeder.kt
+++ src/oop_00000111047_AndreasRahardian/week12/SmartFeeder.kt
@@ -12,7 +12,7 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     return availableGram - requestedGram
 }
 
-fun main{
+fun main() {
     var currentKibbleStock = 50
 
     try {
